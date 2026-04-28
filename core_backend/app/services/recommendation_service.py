ebf69d1e71a5524a58b3fb063553dd8ff15d8f6f
CheemsHCMUS <24120306@student.hcmus.edu.vn>
2026-04-28 22:17:45 +0700

    fix(be): cập nhật theo cons dim

--- core_backend/app/services/recommendation_service.py
+++ core_backend/app/services/recommendation_service.py
@@ -3,6 +3,7 @@ from app.services.user_services import get_user_allergies, get_user_preferences_
 from app.services.allergy_filter import filter_allergy, handle_fallback
 from app.services.candidate_mapper import to_candidates
 from app.domains.ranking.ranking_service import RankingService
+from app.core.config import settings
 
 
 # TODO: Replace with real implementation that queries the database.
@@ -17,31 +18,31 @@ def generate_candidates(query: str):
             "id": 1,
             "name": "Mì Cay Sasin - Làng Đại Học",
             "ingredients": ["tôm", "ớt", "mì", "hải sản"],
-            "vector": [0.1] * 128
+            "vector": [0.1] * settings.VECTOR_DIM
         },
         {
             "id": 2,
             "name": "Mì Cay Seoul - Dĩ An",
             "ingredients": ["thịt bò", "mì", "ớt", "trân châu"],
-            "vector": [0.2] * 128
+            "vector": [0.2] * settings.VECTOR_DIM
         },
         {
             "id": 3,
             "name": "Mì Cay Naga - Làng Đại Học",
             "ingredients": ["hải sản", "mì", "ớt"],
-            "vector": [0.15] * 128
+            "vector": [0.15] * settings.VECTOR_DIM
         },
         {
             "id": 4,
             "name": "Yagami - Ẩm Thực Lẩu Thái-Nhật-Hàn",
             "ingredients": ["bạch tuộc", "mì", "ớt"],
-            "vector": [0.3] * 128
+            "vector": [0.3] * settings.VECTOR_DIM
         },
         {
             "id": 5,
             "name": "Mì Cay Sasin Hoàng Diệu 2",
             "ingredients": ["tôm", "ớt", "mì", "thịt bò"],
-            "vector": [0.12] * 128
+            "vector": [0.12] * settings.VECTOR_DIM
         }
     ]
 
@@ -52,10 +53,6 @@ def recommend(query: str, user_id: str, db: Session):
 
     user_allergies = get_user_allergies(db, user_id)
     user_vector = get_user_preferences_vector(db, user_id)
-
-    # Nếu user chưa có vector (chưa onboarding hoặc lỗi), dùng vector mặc định
-    if user_vector is None:
-        user_vector = [1.0] * 128
 
     raw_candidates = generate_candidates(query)
 
@@ -79,6 +76,9 @@ def recommend(query: str, user_id: str, db: Session):
 
     # 🔥 convert
     candidates = to_candidates(safe_raw)
+
+    if user_vector is None:
+        user_vector = [1.0] * settings.VECTOR_DIM
 
     ranked_ids = ranking_service.rank(
         pref_vector=user_vector,
