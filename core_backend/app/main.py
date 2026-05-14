0f0e6e998edd448da737c982b6ae7386112ae258
CheemsHCMUS <24120306@student.hcmus.edu.vn>
2026-05-14 21:37:55 +0700

    xóa dòng này thì chạy đươc

--- core_backend/app/main.py
+++ core_backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import check_db_connection, engine, Base
-from app.domains.search.models import SearchSession
 from app.domains.users.models import Base as UserBase
 from app.services.ai_client import get_ai_client
 
@@ -42,12 +41,10 @@ async def get_health_status():
 from app.domains.search.router import router as search_router
 from app.domains.users.router import router as users_router
 from app.domains.ranking.router import router as ml_router
-from app.domains.restaurants.router import router as restaurants_router
 
 app.include_router(search_router, prefix="/api/v1", tags=["Search"])
 app.include_router(users_router, prefix="/api/v1/users", tags=["Users"])
 app.include_router(ml_router, prefix="/api/v1", tags=["ML"])
-app.include_router(restaurants_router, prefix="/api/v1", tags=["Restaurants"])
 
 @app.get("/")
 def read_root():
