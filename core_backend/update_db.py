bf91b7232fd0b95bee8eccecc991158b305ff9cd
KMA-K <baoanisa0001@gmail.com>
2026-05-25 01:56:23 +0700

    rename: change friend_requests database table name to user_friend_requests

--- core_backend/update_db.py
+++ core_backend/update_db.py
@@ -70,7 +70,7 @@ def update_schema():
             reaction VARCHAR,
             created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
         );""",
-        """CREATE TABLE IF NOT EXISTS friend_requests (
+        """CREATE TABLE IF NOT EXISTS user_friend_requests (
             id UUID PRIMARY KEY,
             sender_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
             receiver_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
